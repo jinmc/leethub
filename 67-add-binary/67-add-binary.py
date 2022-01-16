fc097d020a60523f888e118c14ad3258b8498192
jinmc <jinmo55555@gmail.com>
2022-01-17 01:53:33 +0900

    Time: 24 ms (98.84%), Space: 14.4 MB (21.19%) - LeetHub

--- 67-add-binary/67-add-binary.py
+++ 67-add-binary/67-add-binary.py
@@ -1,19 +1,14 @@
 class Solution:
     def addBinary(self, a: str, b: str) -> str:
-        a = list(a)
-        b = list(b)
-        carry = 0
-        result = []
-        while a or b or carry:
-            if a:
-                carry += int(a.pop())
-            if b:
-                carry += int(b.pop())
+        def make_dec(num):
+            target = 0
+            for i in range(len(num)):
+                target *= 2
+                target += int(num[i])
+            return target
             
-            num = carry % 2
-            if carry >= 2:
-                carry = 1
-            else:
-                carry = 0
-            result.append(str(num))
-        return "".join(result)[::-1]
+        num_a, num_b = make_dec(a), make_dec(b)
+        # print(num_a, num_b)
+        bin_num =  bin(num_a + num_b)
+        # print(str(bin_num)[2:])
+        return str(bin_num)[2:]
