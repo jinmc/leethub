91dd33cc1a81a3eed3a398df0caa154267eec626
jinmc <jinmo55555@gmail.com>
2022-01-17 22:28:39 +0900

    Time: 56 ms (38.90%), Space: 14.3 MB (41.35%) - LeetHub

--- 69-sqrtx/69-sqrtx.py
+++ 69-sqrtx/69-sqrtx.py
@@ -1,3 +1,17 @@
+## x = 1000 mid = 500 | 250000
+
 class Solution:
     def mySqrt(self, x: int) -> int:
-        return int(x ** 0.5)
+        if x < 2:
+            return x
+        
+        l, r = 0, x
+        while l <= r:
+            mid = (l+r) // 2
+            if mid ** 2 == x or (mid ** 2 < x and (mid+1) ** 2 > x):
+                return mid
+            elif mid ** 2 > x:
+                r = mid - 1
+            else: # mid ** 2 < x
+                l = mid + 1
+        print("exception!")
