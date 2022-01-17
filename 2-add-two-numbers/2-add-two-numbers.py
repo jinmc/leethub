7631f0ec526e6a596f81e2d6e3ac4e6526c4201b
jinmc <jinmo55555@gmail.com>
2022-01-17 23:04:43 +0900

    Time: 72 ms (65.62%), Space: 14.3 MB (72.75%) - LeetHub

--- 2-add-two-numbers/2-add-two-numbers.py
+++ 2-add-two-numbers/2-add-two-numbers.py
@@ -5,37 +5,16 @@
 #         self.next = next
 class Solution:
     def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
-        dummyNode = ListNode()
-        runnerNode = dummyNode
-        leftover = 0
-        while l1 is not None or l2 is not None or leftover > 0:
-            if l1 is None and l2 is None and leftover == 1:
-                runnerNode.next = ListNode(val = 1)
-                leftover = 0
-            elif l1 is None:
-                if leftover + l2.val >=10:
-                    runnerNode.next = ListNode(val = leftover + l2.val - 10)
-                    leftover = 1
-                else:
-                    runnerNode.next = ListNode(val = leftover + l2.val)
-                    leftover = 0
-                l2 = l2.next
-            elif l2 is None:
-                if leftover + l1.val >= 10:
-                    runnerNode.next = ListNode(val = leftover + l1.val - 10)
-                    leftover = 1
-                else:
-                    runnerNode.next = ListNode(val = leftover + l1.val)
-                    leftover = 0
+        dummyNode = runnerNode = ListNode()
+        carry = 0
+        while l1 or l2 or carry:
+            if l1:
+                carry += l1.val
                 l1 = l1.next
-            else:
-                if leftover + l1.val + l2.val >= 10:
-                    runnerNode.next = ListNode(val= leftover + l1.val + l2.val - 10)
-                    leftover = 1
-                else:
-                    runnerNode.next = ListNode(val=leftover + l1.val + l2.val)
-                    leftover = 0
-                l1 = l1.next
+            if l2:
+                carry += l2.val
                 l2 = l2.next
+            runnerNode.next = ListNode(carry % 10)
             runnerNode = runnerNode.next
+            carry //= 10
         return dummyNode.next
