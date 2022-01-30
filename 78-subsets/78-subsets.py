2d45f5e0b820e775d3e12976afb9415bc272518a
jinmc <jinmo55555@gmail.com>
2022-01-30 11:18:08 +0900

    Time: 54 ms (26.42%), Space: 14.1 MB (97.05%) - LeetHub

--- 78-subsets/78-subsets.py
+++ 78-subsets/78-subsets.py
@@ -1,17 +1,12 @@
 class Solution:
     def subsets(self, nums: List[int]) -> List[List[int]]:
-        n = len(nums)
-        output = [[]]
-        for num in nums:
-            temp = []
-            for curr in output: # curr = []
-                # curr.extend([num])
-                # output.append(curr)
-                curr_temp = list(curr)
-                curr_temp.extend([num]) # curr = [1]
-                temp.append(curr_temp) # temp = [[1]]
-            # temp = [curr + [num] for curr in output]
-            # output.append(temp)
-            output.extend(temp) # output = [[], [[1]]]
-            print(temp, output)
-        return output
+        def dfs(nums, res, path):
+            if not nums:
+                res.append(path)
+                return
+            dfs(nums[1:], res, path) # ignore the current element
+            dfs(nums[1:], res, path+[nums[0]]) # select the current element
+        
+        res  = []
+        dfs(nums, res, [])
+        return res
