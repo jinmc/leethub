7a8cdb6d987e0d962bbd22448a1fa97103122c07
jinmc <jinmo55555@gmail.com>
2022-02-08 07:46:30 +0900

    Time: 1477 ms (48.24%), Space: 13.8 MB (98.82%) - LeetHub

--- 5-longest-palindromic-substring/5-longest-palindromic-substring.py
+++ 5-longest-palindromic-substring/5-longest-palindromic-substring.py
@@ -1,20 +1,26 @@
 class Solution:
     def longestPalindrome(self, s):
-        res = ""
-        for i in range(len(s)):
-            # odd case, like "aba"
-            tmp = self.helper(s, i, i)
-            if len(tmp) > len(res):
-                res = tmp
-            # even case, like "abba"
-            tmp = self.helper(s, i, i+1)
-            if len(tmp) > len(res):
-                res = tmp
-        return res
-
-    # get the longest palindrome, l, r are the middle indexes   
-    # from inner to outer
-    def helper(self, s, l, r):
-        while l >= 0 and r < len(s) and s[l] == s[r]:
-            l -= 1; r += 1
-        return s[l+1:r]
+        n = len(s)
+        ret, cnt = "", 0
+        for i in range(n):
+            
+            # 홀수개
+            l, r, this_cnt = i, i, 1
+            while l >= 0 and r < n and s[l] == s[r]:
+                if this_cnt > cnt:
+                    ret = s[l:r+1]
+                    cnt = this_cnt
+                l -= 1
+                r += 1
+                this_cnt += 2
+            
+            # 짝수개
+            l, r, this_cnt = i, i+1, 2
+            while l >= 0 and r < n and s[l] == s[r]:
+                if this_cnt > cnt:
+                    ret = s[l:r+1]
+                    cnt = this_cnt
+                l -= 1
+                r += 1
+                this_cnt += 2
+        return ret
