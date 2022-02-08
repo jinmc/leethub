1ab7e67895131218211ce66b89a6c2fef1a3404e
jinmc <jinmo55555@gmail.com>
2022-02-09 00:39:15 +0900

    Time: 1956 ms (34.34%), Space: 13.8 MB (99.91%) - LeetHub

--- 5-longest-palindromic-substring/5-longest-palindromic-substring.py
+++ 5-longest-palindromic-substring/5-longest-palindromic-substring.py
@@ -1,26 +1,25 @@
 class Solution:
-    def longestPalindrome(self, s):
+    def longestPalindrome(self, s: str) -> str:
+        def isPalin(s1):
+            if s1[::-1] == s1:
+                return True
+            return False
+        # abba
+        ret = ""
         n = len(s)
-        ret, cnt = "", 0
         for i in range(n):
-            
-            # 홀수개
-            l, r, this_cnt = i, i, 1
+            l, r = i, i
             while l >= 0 and r < n and s[l] == s[r]:
-                if this_cnt > cnt:
+                if r-l+1 > len(ret):
                     ret = s[l:r+1]
-                    cnt = this_cnt
                 l -= 1
                 r += 1
-                this_cnt += 2
-            
-            # 짝수개
-            l, r, this_cnt = i, i+1, 2
+                
+            l, r = i, i+1
             while l >= 0 and r < n and s[l] == s[r]:
-                if this_cnt > cnt:
+                if r-l+1 > len(ret): # 새로운 palindrome이 ret보다 길면
                     ret = s[l:r+1]
-                    cnt = this_cnt
                 l -= 1
                 r += 1
-                this_cnt += 2
         return ret
+                    
