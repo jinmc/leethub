4dff893f3781410e99ff1c85baae43b7d9ebe02f
jinmc <jinmo55555@gmail.com>
2022-02-17 02:09:51 +0900

    Time: 6385 ms (8.94%), Space: 18.1 MB (26.98%) - LeetHub

--- 15-3sum/15-3sum.py
+++ 15-3sum/15-3sum.py
@@ -1,23 +1,19 @@
 class Solution:
     def threeSum(self, nums):
-        res = []
+        # O(n^3) -> O(n^2)
+        if len(nums) < 3:
+            return []
         nums.sort()
+        result = set()
         for i in range(len(nums)-2):
-            if i > 0 and nums[i] == nums[i-1]:
-                continue
-            l, r = i+1, len(nums)-1
-            while l < r:
-                s = nums[i] + nums[l] + nums[r]
-                if s < 0:
-                    l +=1 
-                elif s > 0:
-                    r -= 1
-                else:
-                    res.append((nums[i], nums[l], nums[r]))
-                    while l < r and nums[l] == nums[l+1]:
-                        l += 1
-                    while l < r and nums[r] == nums[r-1]:
-                        r -= 1
-                    l += 1; r -= 1
-        return res
-                
+            this_set = set()
+            
+            for j in range(i+1, len(nums)):
+                # nums[i], this_num, nums[j]
+                # nums[i] + this_num + nums[j] = 0
+                # - this_num = nums[i] + nums[j]
+                if -(nums[i]+nums[j]) in this_set:
+                    result.add((nums[i], -(nums[i] + nums[j]), nums[j]))
+                this_set.add(nums[j])
+        # print(result)        
+        return list(result)
