6cd50662fc7cb38342eccfb066b7615f7dda055e
jinmc <jinmo55555@gmail.com>
2022-02-17 23:06:10 +0900

    Time: 39 ms (55.12%), Space: 13.9 MB (94.21%) - LeetHub

--- 17-letter-combinations-of-a-phone-number/17-letter-combinations-of-a-phone-number.py
+++ 17-letter-combinations-of-a-phone-number/17-letter-combinations-of-a-phone-number.py
@@ -2,13 +2,17 @@ class Solution:
     def letterCombinations(self, digits: str) -> List[str]:
         my_dict = {"2":"abc","3":"def","4":"ghi","5":"jkl","6":"mno",
                   "7":"pqrs","8":"tuv","9":"wxyz"}
-        if not digits:
-            return []
-        result = [""]
-        for d in digits:
-            q = list(result) # [""] ["a","b","c"] ["ad","ae",...]
-            result = []
-            for s in q:
-                for c in my_dict[d]:
-                    result.append(s+c)
+        result = []
+        def backtrack(i, s):
+            # base case
+            if i == len(digits):
+                result.append(s)
+                return
+            # recursion, backtracking
+            # else:
+            for c in my_dict[digits[i]]:
+                backtrack(i+1,s + c)
+        
+        if digits:
+            backtrack(0, "")
         return result
