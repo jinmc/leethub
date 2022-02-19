34ad81081d4d897e094143fa8faf7ebb59d76c9e
jinmc <jinmo55555@gmail.com>
2022-02-19 17:33:23 +0900

    Time: 148 ms (83.72%), Space: 14.1 MB (71.20%) - LeetHub

--- 18-4sum/18-4sum.py
+++ 18-4sum/18-4sum.py
@@ -30,19 +30,14 @@ class Solution:
 
         def twoSum(nums: List[int], target: int) -> List[List[int]]:
             res = []
-            lo, hi = 0, len(nums) - 1
+            s = set()
     
-            while (lo < hi):
-                curr_sum = nums[lo] + nums[hi]
-                if curr_sum < target or (lo > 0 and nums[lo] == nums[lo - 1]):
-                    lo += 1
-                elif curr_sum > target or (hi < len(nums) - 1 and nums[hi] == nums[hi + 1]):
-                    hi -= 1
-                else:
-                    res.append([nums[lo], nums[hi]])
-                    lo += 1
-                    hi -= 1
-                                                         
+            for i in range(len(nums)):
+                if len(res) == 0 or res[-1][1] != nums[i]:
+                    if target - nums[i] in s:
+                        res.append([target - nums[i], nums[i]])
+                s.add(nums[i])
+    
             return res
 
         nums.sort()
