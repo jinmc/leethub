d5f5f965537555b58e7292d22efc40fadb91a5ac
jinmc <jinmo55555@gmail.com>
2022-02-23 20:12:18 +0900

    Time: 1895 ms (26.08%), Space: 13.9 MB (97.36%) - LeetHub

--- 18-4sum/18-4sum.py
+++ 18-4sum/18-4sum.py
@@ -1,48 +1,20 @@
 class Solution:
     def fourSum(self, nums: List[int], target: int) -> List[List[int]]:
-	
-        def kSum(nums: List[int], target: int, k: int) -> List[List[int]]:
-            res = []
-            
-            # If we have run out of numbers to add, return res.
-            if not nums:
-                return res
-            
-            # There are k remaining values to add to the sum. The 
-            # average of these values is at least target // k.
-            average_value = target // k
-            
-            # We cannot obtain a sum of target if the smallest value
-            # in nums is greater than target // k or if the largest 
-            # value in nums is smaller than target // k.
-
-            
-            if k == 2:
-                return twoSum(nums, target)
-    
-            for i in range(len(nums)):
-                if i == 0 or nums[i - 1] != nums[i]:
-                    for subset in kSum(nums[i + 1:], target - nums[i], k - 1):
-                        res.append([nums[i]] + subset)
-    
-            return res
-
-        def twoSum(nums: List[int], target: int) -> List[List[int]]:
-            res = []
-            lo, hi = 0, len(nums) - 1
-    
-            while (lo < hi):
-                curr_sum = nums[lo] + nums[hi]
-                if curr_sum < target or (lo > 0 and nums[lo] == nums[lo - 1]):
-                    lo += 1
-                elif curr_sum > target or (hi < len(nums) - 1 and nums[hi] == nums[hi + 1]):
-                    hi -= 1
-                else:
-                    res.append([nums[lo], nums[hi]])
-                    lo += 1
-                    hi -= 1
-                                                         
-            return res
-
         nums.sort()
-        return kSum(nums, target, 4)
+        result = set()
+        for a in range(len(nums)-3):
+            for b in range(a+1, len(nums)-2):
+                
+                l, r = b+1, len(nums)-1
+                while l < r:
+                    this_sum = nums[a] + nums[b] + nums[l] + nums[r]
+                    if this_sum == target:
+                        result.add((nums[a], nums[b], nums[l], nums[r]))
+                        l += 1
+                        r -= 1
+                    elif this_sum < target:
+                        l += 1
+                    else:
+                        r -= 1
+                    
+        return list(result)
