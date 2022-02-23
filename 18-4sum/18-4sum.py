4c5bdee76c180663b4abc0cbcb8fb4f5289219fd
jinmc <jinmo55555@gmail.com>
2022-02-23 21:33:19 +0900

    Time: 3131 ms (7.96%), Space: 13.8 MB (97.36%) - LeetHub

--- 18-4sum/18-4sum.py
+++ 18-4sum/18-4sum.py
@@ -3,13 +3,12 @@ class Solution:
         nums.sort()
         result = set()
         for a in range(len(nums)-3):
-            for b in range(a+1, len(nums)-2):
+            for b in range(a+1,len(nums)-2):
+                # nums[a] + nums[b] + nums[c] + nums[d] == target
                 this_dict = {}
-                # nums[a] + nums[b] + nums[c] + nums[d] = target
                 for c in range(b+1, len(nums)):
-                    if (target - nums[a] - nums[b] - nums[c]) in this_dict:
-                        result.add((nums[a], nums[b], nums[c], target - nums[a] - nums[b] - nums[c]))
+                    if (target-nums[a]-nums[b]-nums[c]) in this_dict:
+                        result.add((nums[a], nums[b], nums[c], target-nums[a]-nums[b]-nums[c]))
                     else:
-                        this_dict[nums[c]] = target - nums[a] - nums[b] - nums[c]
-                    
+                        this_dict[nums[c]] = target-nums[a]-nums[b]-nums[c]
         return list(result)
