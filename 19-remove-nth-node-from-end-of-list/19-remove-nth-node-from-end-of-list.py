aa5d51839366dc4126fee24162a6fdde91f978d9
jinmc <jinmo55555@gmail.com>
2022-03-07 22:10:11 +0900

    Time: 52 ms (40.18%), Space: 13.7 MB (99.10%) - LeetHub

--- 19-remove-nth-node-from-end-of-list/19-remove-nth-node-from-end-of-list.py
+++ 19-remove-nth-node-from-end-of-list/19-remove-nth-node-from-end-of-list.py
@@ -5,18 +5,28 @@
 #         self.next = next
 class Solution:
     def removeNthFromEnd(self, head: Optional[ListNode], n: int) -> Optional[ListNode]:
+        # 공통
         dummy = ListNode(-1)
         dummy.next = head
         
-        runner1 = runner2 = dummy
+        # 노드 숫자 세기
+        cnt = 0
+        runner = head
+        while runner:
+            runner = runner.next
+            cnt += 1
+            
+        # 몇번째 에서 뺄지 정함
+        cnt -= n
+        this_cnt = 0
+        runner = dummy
         
-        for _ in range(n+1):
-            runner2 = runner2.next
-        
-        while runner2:
-            runner1 = runner1.next
-            runner2 = runner2.next
-        
-        runner1.next = runner1.next.next
-        
+        while runner:
+            if this_cnt == cnt:
+                runner.next = runner.next.next
+                break
+            this_cnt += 1
+            runner = runner.next
+            
+                
         return dummy.next
