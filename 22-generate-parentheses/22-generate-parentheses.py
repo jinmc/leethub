b9cdd6a3aadbda9f72fef99c4cb699117271224d
jinmc <jinmo55555@gmail.com>
2022-03-13 11:17:37 +0900

    Time: 45 ms (66.44%), Space: 14.2 MB (85.57%) - LeetHub

--- 22-generate-parentheses/22-generate-parentheses.py
+++ 22-generate-parentheses/22-generate-parentheses.py
@@ -1,28 +1,17 @@
 class Solution:
     def generateParenthesis(self, n: int) -> List[str]:
-        # [()()]
-        # [(()]
-        # [((())]
-        def generate(A = []):
-            if len(A) == 2*n:
-                if valid(A):
-                    ans.append("".join(A))
-            else:
-                A.append('(')
-                generate(A)
-                A.pop()
-                A.append(')')
-                generate(A)
-                A.pop()
-
-        def valid(A):
-            bal = 0
-            for c in A:
-                if c == '(': bal += 1
-                else: bal -= 1
-                if bal < 0: return False
-            return bal == 0
-
         ans = []
-        generate()
+        def backtrack(S = [], left = 0, right = 0):
+            if len(S) == 2 * n:
+                ans.append("".join(S))
+                return
+            if left < n:
+                S.append("(")
+                backtrack(S, left+1, right)
+                S.pop()
+            if right < left:
+                S.append(")")
+                backtrack(S, left, right+1)
+                S.pop()
+        backtrack()
         return ans
